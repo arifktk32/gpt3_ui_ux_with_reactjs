4256bcbd952aec2fc7a21105260199a478d9de29
Arif Iqbal <arifktk32@gmail.com>
2023-06-11 21:14:08 +0500

    Deleted src folder and created new with components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CTA from './components/cta/CTA';
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Article />
+        <Brand />
+        <CTA />
+        <Feature />
+        <Navbar />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Brand = () => {
+  return (
+    <div>Brand</div>
+  )
+}
+
+export default Brand;

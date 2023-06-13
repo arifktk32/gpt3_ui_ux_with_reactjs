b85163f142346f18d186abc74a596f6be44f6b49
Arif Iqbal <arifktk32@gmail.com>
2023-06-13 23:56:20 +0500

    WIP Styling Header component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 export const Navbar = () => (
-  <div className='container navbar__wrapper'>
-    <div className="navbar__wrapper-left">
-      <a className='logo ' href='#'><img src={logo} alt='GPT3 logo' /></a>
-      <ul>
+  <div className='container flex navbar__wrapper'>
+    <div className="flex navbar__wrapper-left">
+      <a className='logo' href='#'><img src={logo} alt='GPT3 logo' /></a>
+      <ul className='flex'>
         <li><a href='#'>Home</a></li>
         <li><a href='#'>What is GPT?</a></li>
         <li><a href='#'>Open AI</a></li>
@@ -15,7 +15,7 @@ export const Navbar = () => (
       </ul>
     </div>
     <div className="navbar__wrapper-right">
-      <ul>
+      <ul className='flex'>
         <li><a href='#'>Sign in</a></li>
         <li className='active'><a href='#'>Sign up</a></li>
       </ul>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = () =>  (
+  <div className='container header__banner'>
+    <h1 className='gradient-text header__banner-title'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+    <p className='header__banner-para'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+    <form name='request-access' id='request-access'>
+      <input type='email' name='request-access-email' placeholder='Your Email Address' />
+      <button type='submit'>Get Started</button>
+    </form>
+    <div className='flex header__banner-people'>
+      <img src={people} alt='Image of People that already requested access' />
+      <p>1,600 people requested access a visit in last 24 hours</p>
+    </div>
+    <div className='header__banner-image'>
+      <img src={ai} alt='Banner Image' />
+    </div>
+  </div>
+)
 
 export default Header

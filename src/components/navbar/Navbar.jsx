8b7619af64c95fb4584c8bebafae0cf37a982053
Arif Iqbal <arifktk32@gmail.com>
2023-06-14 18:34:08 +0500

    Re - Organize assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/images/logo.svg';
 import './navbar.scss';
 
 export const Navbar = () => (
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
+import people from '../../assets/images/people.png';
+import ai from '../../assets/images/ai.png';
 import './header.scss';
 
 const Header = () =>  (

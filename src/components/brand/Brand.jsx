2aa1f5235aefb06f362828be9cc5aece996464a9
Arif Iqbal <arifktk32@gmail.com>
2023-06-15 01:00:06 +0500

    Design Brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './brand.scss';
+import { google, slack, atlassian, dropbox, shopify } from './logos.js'
 
-export const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+export const Brand = () => (
+  <div className='container brands'>
+    <ul className='flex'>
+      <li>
+        <a href='#'>
+          <img src={google} alt='Google logo' />
+        </a>
+      </li>
+      <li>
+        <a href='#'>
+          <img src={slack} alt='Slack logo' />
+        </a>
+      </li>
+      <li>
+        <a href='#'>
+          <img src={atlassian} alt='Atlassian logo' />
+        </a>
+      </li>
+      <li>
+        <a href='#'>
+          <img src={dropbox} alt='Dropbox logo' />
+        </a>
+      </li>
+      <li>
+        <a href='#'>
+          <img src={shopify} alt='Shopify logo' />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
 
 export default Brand;

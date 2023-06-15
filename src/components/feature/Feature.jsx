8ec9a9dc9b3f31f9c581835ce19fedb244b2deb7
Arif Iqbal <arifktk32@gmail.com>
2023-06-16 00:48:08 +0500

    WIP - Style WhatGPT3 component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './feature.scss';
 
-export const Feature = () => {
-  return (
-    <div>Feature</div>
-  )
-}
+export const Feature = ({ title, text }) => (
+  <div className='flex feature'>
+    <h1 className='feature__title'> {title} </h1>
+    <p className='feature__text'> {text} </p>
+  </div>
+)
 
 export default Feature;

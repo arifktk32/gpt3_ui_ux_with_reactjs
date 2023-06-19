8fd8455a361ffda8113851b3ccbdb79986902335
Arif Iqbal <arifktk32@gmail.com>
2023-06-19 16:15:43 +0500

    Show features by looping through dynamic data array

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Feature } from '../../components';
 import './whatgpt3.scss';
+import featuresData from './features.js';
 
 const WhatGPT3 = () => (
   <div className='container wgpt3'>
     <div className='wgpt3__row1'>
-      <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
+      <Feature title={featuresData[0].title} text={featuresData[0].text} />
     </div>
     <div className='flex wgpt3__row2'>
       <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
       <p>Explore The Library</p>
     </div>
     <div className='flex wgpt3__row3'>
-      <Feature title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. ' />
-      <Feature title='Knowledgebase' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b' />
-      <Feature title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b' />
+      {featuresData.slice(1).map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
     </div>
   </div>
 )
---
+++ src/containers/whatGPT3/features.js
@@ -0,0 +1,20 @@
+const featursData = [
+    {
+        "title": "What is GPT-3",
+        "text": "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+    },
+    {
+        "title": "Chatbots",
+        "text": "We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+    },
+    {
+        "title": "Knowledgebase",
+        "text": "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+    },
+    {
+        "title": "Education",
+        "text": "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+    },
+];
+
+export default featursData;

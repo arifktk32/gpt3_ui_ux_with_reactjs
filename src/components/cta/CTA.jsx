cfff84b0321069686e0cc120936d0569d1482017
Arif Iqbal <arifktk32@gmail.com>
2023-06-24 00:21:49 +0500

    Complete designing the CTA component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./cta.scss";
 
-export const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+export const CTA = () => (
+  <div className="container flex cta">
+    <div className="cta__content">
+      <p className="cta__subtitle">Register today &amp; start exploring the endless possiblities.</p>
+      <p className="cta__title">Request Early Access to Get Started</p>
+    </div>
+    <div className="cta__btn">
+      <button>Get Started</button>
+    </div>
+  </div>
+)
 
 export default CTA;

b12eb8f442c1a292046ae34de1e423a4c6d02f86
Arif Iqbal <arifktk32@gmail.com>
2023-07-02 18:22:09 +0500

    Add responsive mobile toggle menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
-import { RiMenu3Line } from 'react-icons/ri';
+import { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/images/logo.svg';
 import './navbar.scss';
 
-export const Navbar = () => (
-  <div className='container flex navbar'>
-    <div className="flex navbar__left">
-      <a className='logo' href='#'><img src={logo} alt='GPT3 logo' /></a>
-      <ul className='flex'>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>What is GPT?</a></li>
-        <li><a href='#'>Open AI</a></li>
-        <li><a href='#'>Case Studies</a></li>
-        <li><a href='#'>Library</a></li>
-      </ul>
+export const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const Menu = () => (
+    <ul className='flex'>
+      <li><a href='#'>Home</a></li>
+      <li><a href='#'>What is GPT?</a></li>
+      <li><a href='#'>Open AI</a></li>
+      <li><a href='#'>Case Studies</a></li>
+      <li><a href='#'>Library</a></li>
+    </ul>
+  );
+
+  return (
+    <div className='container flex navbar'>
+      <div className="flex navbar__left">
+        <a className='logo' href='#'><img src={logo} alt='GPT3 logo' /></a>
+        <Menu />
+      </div>
+      <div className="flex navbar__right">
+        <ul className='flex navbar__right-account'>
+          <li><a href='#'>Sign in</a></li>
+          <li className='active'><a href='#'>Sign up</a></li>
+        </ul>
+        {toggleMenu ? <RiCloseLine className='navbar__hamburger-button' color='#fff' size='27' onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line className='navbar__hamburger-button' color='#fff' size='27' onClick={() => setToggleMenu(true)} />}
+
+        {toggleMenu && (
+          <div className='navbar__mobilemenu'>
+            <Menu />
+            <ul className='navbar__mobilemenu-sign'>
+              <li><a href='#'>Sign in</a></li>
+              <li className='active'><a href='#'>Sign up</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
-    <div className="flex navbar__right">
-      <ul className='flex navbar__right-account'>
-        <li><a href='#'>Sign in</a></li>
-        <li className='active'><a href='#'>Sign up</a></li>
-      </ul>
-      <RiMenu3Line className='navbar__hamburger-button' color='#fff' size='27' style={{marginLeft: "1rem"}} />
-    </div>
-  </div>
-)
+  )
+}
 
 export default Navbar;
